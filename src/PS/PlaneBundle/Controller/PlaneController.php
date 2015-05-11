f7dfc2c5722907136d53792559d62bf851ddc898
Duy-Laurent TRAN <duylaurent.tran@gmail.com>
2015-05-11 21:18:51 +0200

    added createAction method. Planes can now be created.

--- src/PS/PlaneBundle/Controller/PlaneController.php
+++ src/PS/PlaneBundle/Controller/PlaneController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class PlaneController extends Controller
 {
@@ -26,7 +27,30 @@ class PlaneController extends Controller
      */
     public function createAction(Request $request)
     {
-        // TODO
+        $plane = new Plane();
+        $planeForm = $this->createForm(new PlaneType(), $plane);
+
+        $planeForm->submit($request);
+        if (!$planeForm->isValid()) {
+
+            $formErrors = $this->getFormErrorMessage($planeForm);
+
+            return new Response(
+                empty($formErrors['error']) ? "" : json_encode($formErrors),
+                Response::HTTP_BAD_REQUEST,
+                array('Content-Type' => 'application/json')
+            );
+        }
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($plane);
+        $em->flush();
+
+        return new Response(
+            json_encode($plane->toArray()),
+            Response::HTTP_CREATED,
+            array('Content-Type' => 'application/json')
+        );
     }
 
     /**
